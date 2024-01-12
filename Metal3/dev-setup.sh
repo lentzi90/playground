bb328d4025522f1f76c9a77b6da0e7431586a35a
Huy Mai <huy.mai@est.tech>
2024-01-12 10:25:16 +0000

    Add password auth to bmo and ironic kustomizations

--- Metal3/dev-setup.sh
+++ Metal3/dev-setup.sh
@@ -30,6 +30,32 @@ docker run --name image-server-e2e -d \
 
 kubectl create namespace baremetal-operator-system
 
+export IRONIC_USERNAME="$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 12 | head -n 1)"
+export IRONIC_PASSWORD="$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 12 | head -n 1)"
+export IRONIC_INSPECTOR_USERNAME="$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 12 | head -n 1)"
+export IRONIC_INSPECTOR_PASSWORD="$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 12 | head -n 1)"
+
+BMO_OVERLAY=${REPO_ROOT}/Metal3/bmo
+IRONIC_OVERLAY=${REPO_ROOT}/Metal3/ironic
+
+echo "${IRONIC_USERNAME}" > "${BMO_OVERLAY}/ironic-username"
+echo "${IRONIC_PASSWORD}" > "${BMO_OVERLAY}/ironic-password"
+echo "${IRONIC_INSPECTOR_USERNAME}" > "${BMO_OVERLAY}/ironic-inspector-username"
+echo "${IRONIC_INSPECTOR_PASSWORD}" > "${BMO_OVERLAY}/ironic-inspector-password"
+
+curl -O https://raw.githubusercontent.com/metal3-io/baremetal-operator/main/ironic-deployment/components/basic-auth/ironic-auth-config-tpl
+curl -O https://raw.githubusercontent.com/metal3-io/baremetal-operator/main/ironic-deployment/components/basic-auth/ironic-inspector-auth-config-tpl
+
+envsubst < "${REPO_ROOT}/ironic-auth-config-tpl" > \
+  "${IRONIC_OVERLAY}/ironic-auth-config"
+envsubst < "${REPO_ROOT}/ironic-inspector-auth-config-tpl" > \
+  "${IRONIC_OVERLAY}/ironic-inspector-auth-config"
+
+echo "IRONIC_HTPASSWD=$(htpasswd -n -b -B "${IRONIC_USERNAME}" "${IRONIC_PASSWORD}")" > \
+  "${IRONIC_OVERLAY}/ironic-htpasswd"
+echo "INSPECTOR_HTPASSWD=$(htpasswd -n -b -B "${IRONIC_INSPECTOR_USERNAME}" \
+  "${IRONIC_INSPECTOR_PASSWORD}")" > "${IRONIC_OVERLAY}/ironic-inspector-htpasswd"
+
 clusterctl init --infrastructure=metal3
 kubectl apply -k "${REPO_ROOT}/Metal3/ironic"
 kubectl apply -k "${REPO_ROOT}/Metal3/bmo"
